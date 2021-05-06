8d5bc2b5b62e5e2ceca9822ed8a56c6f33695626
krishpranav <krisna.pranav@gmail.com>
2021-05-06 14:06:40 +0530

    added function for term box input es

--- gomatrix.go
+++ gomatrix.go
@@ -12,3 +12,38 @@ import (
 
 	"github.com/nsf/termbox-go"
 )
+
+// type
+type Point struct {
+	Head 	bool
+	Char	byte
+	Age		int 
+}
+
+// variables
+var clear map[string]func()
+
+var cols [][]Point
+var width int
+var height int
+
+// main fucntion
+func main() {
+	err := termbox.Init()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer termbox.Close()
+
+	termbox.SetInputMode(termbox.InputEsc)
+	width, height = termbox.Size()
+
+	cols = make2d(width, height)
+
+	data := make([]byte, width*height)
+	rand.Read(data)
+	
+
+}
